b3866c49982c042aaf64c2e344c5a04a6bff1f33
Sebastián Solari <35378033+ssolaric@users.noreply.github.com>
2020-01-31 16:45:51 -0500

    Basic motion algorithm

--- 1.js
+++ 1.js
@@ -1,19 +1,46 @@
-let position;
-let velocity;
+class Mover {
+  constructor() {
+    this.position = createVector(random(width), random(height));
+    this.velocity = createVector(random(-2, 2), random(-2, 2));
+  }
+
+  update() {
+    this.position.add(this.velocity);
+  }
+
+  display() {
+    stroke(0);
+    fill(175);
+    ellipse(this.position.x, this.position.y, 16, 16);
+  }
+
+  checkEdges() {
+    if (this.position.x > width) {
+      this.position.x = 0;
+    }
+    else if (this.position.x < 0) {
+      this.position.x = width;
+    }
+
+    if (this.position.y > height) {
+      this.position.y = 0;
+    }
+    else if (this.position.y < 0) {
+      this.position.y = height;
+    }
+  }
+}
+
+let mover;
 
 function setup() {
   createCanvas(640, 360);
-  position = createVector(100, 100);
-  velocity = createVector(1, 3.3);
+  mover = new Mover();
 }
 
 function draw() {
   background(255);
-  let mouse = createVector(mouseX, mouseY);
-  let center = createVector(width / 2, height / 2);
-  mouse.sub(center);
-  mouse.normalize();
-  mouse.mult(100);
-  translate(width / 2, height / 2);
-  line(0, 0, mouse.x, mouse.y);
+  mover.update();
+  mover.checkEdges();
+  mover.display();
 }
