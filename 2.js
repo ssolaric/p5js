a2e9da6822544ec84086cd9eb9b3aef680bab542
Sebastián Solari <35378033+ssolaric@users.noreply.github.com>
2020-02-01 01:16:11 -0500

    Add drag force

--- 2.js
+++ 2.js
@@ -36,31 +36,63 @@ class Mover {
   applyForce(force) {
     this.acceleration.add(p5.Vector.div(force, this.mass));
   }
+
+  isInside(liquid) {
+    return (
+      this.position.x >= liquid.x &&
+      this.position.x <= liquid.x + liquid.w &&
+      this.position.y >= liquid.y &&
+      this.position.y <= liquid.y + liquid.h
+    );
+  }
+
+  drag(liquid) {
+    let dragMagnitude = liquid.c * this.velocity.magSq();
+    let drag = this.velocity.copy();
+    drag.mult(-1);
+    drag.normalize();
+    drag.mult(dragMagnitude);
+    this.applyForce(drag);
+  }
+}
+
+class Liquid {
+  // c: drag coefficient
+  constructor(x, y, w, h, c) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+  }
+
+  display() {
+    noStroke();
+    fill(175);
+    rect(this.x, this.y, this.w, this.h);
+  }
 }
 
 const NUM_MOVERS = 20;
 let movers = [];
+let liquid;
 
 function setup() {
   createCanvas(640, 360);
   for (let i = 0; i < NUM_MOVERS; i++) {
     movers.push(new Mover(random(0.1, 5), 0, 0));
   }
+  liquid = new Liquid(0, height / 2, width, height / 2, 0.1);
 }
 
 function draw() {
   background(255);
-  const wind = createVector(0.001, 0);
+  liquid.display();
   for (let i = 0; i < NUM_MOVERS; i++) {
+    if (movers[i].isInside(liquid)) {
+      movers[i].drag(liquid);
+    }
     const gravity = createVector(0, 0.1 * movers[i].mass);
-    const mu = 0.01;
-    const friction = movers[i].velocity.copy();
-    friction.mult(-1);
-    friction.normalize();
-    friction.mult(mu);
-
-    movers[i].applyForce(friction);
-    movers[i].applyForce(wind);
     movers[i].applyForce(gravity);
     movers[i].update();
     movers[i].display();
